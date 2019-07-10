e40c829526ce794e1ec93d515a9b55baa143d75f
dodin.roman@gmail.com <dodin.roman@gmail.com>
2019-07-10 13:43:18 +0300

    added CORS handling

--- pycatj-serverless/main.py
+++ pycatj-serverless/main.py
@@ -10,6 +10,19 @@ from pycatj import pycatj
 
 
 def pycatjify(request):
+    # Set CORS headers for the preflight request
+    if request.method == "OPTIONS":
+        # Allows GET requests from any origin with the Content-Type
+        # header and caches preflight response for an 3600s
+        headers = {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "*",
+            "Access-Control-Allow-Headers": "Content-Type",
+            "Access-Control-Max-Age": "3600",
+        }
+
+        return ("", 204, headers)
+
     # default data value
     data = json.loads(
         '{"data":"test_value","somenumber":123,"a_dict":{"asd":"123","qwe":[1,2,3],"nested_dict":{"das":31,"qwe":"asd"}}}'
@@ -31,7 +44,10 @@ def pycatjify(request):
 
     result = io.StringIO()
     pycatj.process_dict(data, root, result)
-    return json.dumps({"data": result.getvalue()})
+
+    # Set CORS headers for the main request
+    headers = {"Access-Control-Allow-Origin": "*"}
+    return (json.dumps({"data": result.getvalue()}), 200, headers)
 
 
 if __name__ == "__main__":
